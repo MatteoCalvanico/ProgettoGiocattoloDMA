dd491ff56d18883e411115ea9d75da5463fef8d0
Matteo Calvanico <matteo.calvanico03gmail.com>
2025-04-08 17:08:26 +0200

    Update reader to read from mongo and return them with api

--- back-end/README.md
+++ back-end/README.md
@@ -1,1 +1,2 @@
-Per far startare il subscriber fare: "npm run sub"
+Per far startare il subscriber fare: "npm run sub".
+Per far startare il reader che espone le api fare: "npm run api".
---
+++ back-end/src/reader.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { Message } from "./model/schema";
+
+const express = require('express')
+const app = express()
+const port = 8080
+
+// Connessione a MongoDB con Mongoose
+const mongoUrl = "mongodb://localhost:27017/projectOne";
+mongoose.connect(mongoUrl)
+  .then(() => console.log('Connection to MongoDB succedes'))
+  .catch(err => console.error('Errore di connessione a MongoDB:', err));
+
+
+// Tutti i messaggi
+app.get('/', async (req, res) => {
+    try {
+        const messages = await Message.find({});
+        res.json(messages);
+    } catch (err) {
+        res.status(500).json({ error: (err as Error).message });
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
